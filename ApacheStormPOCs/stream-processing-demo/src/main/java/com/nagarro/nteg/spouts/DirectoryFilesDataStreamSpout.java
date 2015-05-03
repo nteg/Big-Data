7330b03016d8b920b00a30918e3872310b8ecf09
Amit Kumar <amit.kumar02@nagarro.com>
2015-05-03 16:53:27 +0530

    Amended comments to include licenses

--- ApacheStormPOCs/stream-processing-demo/src/main/java/com/nagarro/nteg/spouts/DirectoryFilesDataStreamSpout.java
+++ ApacheStormPOCs/stream-processing-demo/src/main/java/com/nagarro/nteg/spouts/DirectoryFilesDataStreamSpout.java
@@ -1,5 +1,17 @@
 /**
- * 
+ * Copyright 2015 Nagarro Softwares Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.nagarro.nteg.spouts;
 
@@ -21,7 +33,7 @@ import com.nagarro.nteg.utils.DirectoryFilesDataReader;
 import com.nagarro.nteg.utils.DirectoryFilesDataReaderFactory;
 
 /**
- * @author amitkumar02
+ * @author Nagarro Softwares Pvt. Ltd.
  *
  */
 public class DirectoryFilesDataStreamSpout extends BaseRichSpout{
